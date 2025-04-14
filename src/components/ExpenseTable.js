d1ce7c32299dff97f04744a90bec3e6cd210c105
Nevillemuli <neville.muli@gmail.com>
2025-04-14 19:19:37 +0300

    Initializes components and creates expense table and searchbar

---
+++ src/components/ExpenseTable.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const ExpenseTable = ({ expenses }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Category</th>
+          <th>Amount</th>
+          <th>Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        {expenses.map((expense) => (
+          <tr key={expense.id}>
+            <td>{expense.expenseName}</td>
+            <td>{expense.description}</td>
+            <td>{expense.category}</td>
+            <td>${expense.amount.toFixed(2)}</td>
+            <td>{expense.date}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default ExpenseTable;
---
+++ src/components/Searchbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SearchBar = ({ searchTerm, onSearchChange }) => {
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Search expenses..."
+          value={searchTerm}
+          onChange={(e) => onSearchChange(e.target.value)}
+        />
+      </div>
+    );
+  };
+  
+  export default SearchBar;
